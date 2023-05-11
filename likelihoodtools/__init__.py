d19728548458c84c2bc124ffe8cec15b6430c146
Florian Thomas <fthomas@uni-mainz.de>
2023-05-11 18:33:18 +0200

    Add init

---
+++ likelihoodtools/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+
+Author: F. Thomas
+Date: May 11, 2023
+
+"""
+
+from .plot import make_1d_llh_plot, make_2d_llh_plot
+from .likelihood import AdaptiveLikelihoodScanner, LikelihoodGridScanner, GaussianLikelihoodModel, FunctionFitter, GridFitter
