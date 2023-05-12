0a5a8d6f54d372d985353e75afc061eb7c751c54
Florian Thomas <fthomas@uni-mainz.de>
2023-05-12 18:22:57 +0200

    Add missing import to __init__

--- likelihoodtools/__init__.py
+++ likelihoodtools/__init__.py
@@ -7,4 +7,4 @@ Date: May 11, 2023
 """
 
 from .plot import make_1d_llh_plot, make_2d_llh_plot
-from .likelihood import AdaptiveLikelihoodScanner, LikelihoodGridScanner, GaussianLikelihoodModel, FunctionFitter, GridFitter
+from .likelihood import AdaptiveLikelihoodScanner, LikelihoodGridScanner, GaussianLikelihoodModel, FunctionFitter, GridFitter, add_noise
