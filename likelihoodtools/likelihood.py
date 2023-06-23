Fix the scaling of the std of the complex normal distribution

likelihoodtools/likelihood.py
@@ -42,13 +42,13 @@ class GaussianLikelihoodModel(LikelihoodModel):
     def log_likelihood_pdf(self):
         sigma = self.sigma
         if self.complex_data:
-            sigma = sigma/2
+            sigma = sigma/np.sqrt(2)
         return lambda theta,x: -0.5*np.abs((x - self.f(*theta))/sigma)**2
         
     def gen_noise(self, n):
         
         if self.complex_data:
-             return np.random.normal(loc=0, scale=self.sigma/2, size=(n, 2)).view(np.complex128)
+             return np.random.normal(loc=0, scale=self.sigma/np.sqrt(2), size=(n, 2)).view(np.complex128)
         else:
             return np.random.normal(loc=0, scale=self.sigma, size=n)
 