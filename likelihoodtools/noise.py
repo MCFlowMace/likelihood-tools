23e83b06076f4599fd7691afd310fbdda5c0a338
Florian Thomas <fthomas@uni-mainz.de>
2023-08-02 17:29:14 +0200

    Add module for noise related things

---
+++ likelihoodtools/noise.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+
+def add_noise(data, likelihood_model):
+    
+    n = data.size
+    
+    noise = likelihood_model.gen_noise(n)
+    
+    return data + noise.reshape(data.shape)
+
+
+def thermal_noise_var(T, sr, complex_data=True):
+    
+    R=50
+    kb = 1.380649e-23
+    
+    df = sr if complex_data else sr/2
+    return 4*kb*R*T*df
